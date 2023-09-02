9c571da0f8f0e259ecded1e6973f074564003219
1shChheda <1shvenom786@gmail.com>
2023-09-02 16:32:59 +0530

    initialize ros workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/vansh/ros/project01/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/vansh/ros/ros-course/devel;/home/vansh/ros/my_teleop_ws/devel;/opt/ros/noetic'.split(';') if '/home/vansh/ros/ros-course/devel;/home/vansh/ros/my_teleop_ws/devel;/opt/ros/noetic' != '' else []
